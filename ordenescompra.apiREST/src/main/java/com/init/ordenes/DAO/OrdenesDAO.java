a1d6ace59fc510d6561e9058322e6a395e2cfc4d
luisSebas1802 <92000931+luisSebas1802@users.noreply.github.com>
2021-10-11 20:28:53 -0500

    Delete ordenescompra.apiREST directory

--- ordenescompra.apiREST/src/main/java/com/init/ordenes/DAO/OrdenesDAO.java
+++
@@ -1,9 +0,0 @@
-package com.init.ordenes.DAO;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.init.ordenes.entidad.Ordencompra;
-
-public interface OrdenesDAO  extends JpaRepository<Ordencompra, Long>{
-
-}
